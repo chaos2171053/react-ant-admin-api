13f863c44e4e8f62cc6e33db898e33be77b00111
chaos2171053 <chaos2171053@gmail.com>
2020-02-16 18:30:21 +0800

    improvement(redis): pwd

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -12,6 +12,14 @@ export default () => {
   // config.logger = {
   //   dir: '/home/admin/logs/inyou-server',
   // };
+  config.redis = {
+    client: {
+      port: 6379,
+      host: "127.0.0.1",
+      password: "chaos666",
+      db: 2
+    }
+  };
 
   return config;
 };
