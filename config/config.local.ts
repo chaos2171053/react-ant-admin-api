f37d148385509cb97e836f0982bd364d8728a2aa
chaos2171053 <chaos2171053@gmail.com>
2020-02-16 16:52:58 +0800

    build(tslint,redis`s prod env): remove telint rules,remove redis pwd

--- config/config.local.ts
+++ config/config.local.ts
@@ -1,10 +1,18 @@
-import { EggAppConfig, PowerPartial } from 'egg';
+import { EggAppConfig, PowerPartial } from "egg";
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
   config.static = {
     maxAge: 0,
-    cacheControl: 'no-cache',
+    cacheControl: "no-cache"
+  };
+  config.redis = {
+    client: {
+      port: 6379,
+      host: "127.0.0.1",
+      // password: "123456",
+      db: 2
+    }
   };
   return config;
 };
